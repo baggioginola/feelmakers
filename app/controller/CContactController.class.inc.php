109030f26ebdd7581389f24d102788495dd94f59
baggioginola <mariocuevas88@outlook.com>
2017-05-21 11:52:29 -0500

    - message added, receipt changed

--- app/controller/CContactController.class.inc.php
+++ app/controller/CContactController.class.inc.php
@@ -13,9 +13,10 @@ class Contact extends BaseController
 
     private $parameters = array();
     #private $send_to = 'contacto@grupocoser.com.mx';
-    private $send_to = 'mariocuevas88@gmail.com';
+    private $send_to = 'contacto@feelmakers.mx';
     private $subject = 'Contacto de Pagina Web';
     private $header = 'From:  Sitio Web <info@feelmakers.com.mx>';
+    private $message = '';
 
     private $validParameters = array(
         'email' => TYPE_ALPHA,
@@ -38,6 +39,7 @@ class Contact extends BaseController
             return json_encode($this->getResponse(STATUS_FAILURE_INTERNAL, MESSAGE_ERROR));
         }
 
+        $this->setMessage();
         if(!$this->emailRequest()){
             return json_encode($this->getResponse(STATUS_FAILURE_INTERNAL, MESSAGE_ERROR));
         }
@@ -53,7 +55,15 @@ class Contact extends BaseController
 
         #$this->header .= $this->parameters['email'];
 
-        return mail($this->send_to, $this->subject, $this->parameters['mensaje'], $this->header);
+        return mail($this->send_to, $this->subject, $this->message, $this->header);
+    }
+
+    private function setMessage()
+    {
+        $this->message .= 'Nombre: ' . $this->parameters['nombre'] . "\n";
+        $this->message .= 'E-mail: ' . $this->parameters['email'] . "\n";
+        $this->message .= 'Teléfono: ' . $this->parameters['telefono'] . "\n";
+        $this->message .= 'Mensaje: ' . $this->parameters['mensaje'] . "\n";
     }
 
     /**
